f171d460d2070146787236030e6745fec77e65dd
okata-cell <okataolaniyi@gmail.com>
2026-03-15 20:02:30 +0000

    fix: update NDK version to 27.0.12077973 for plugin compatibility

--- echelle_eg_39/android/app/build.gradle.kts
+++ echelle_eg_39/android/app/build.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.example.echelle_eg_39"
+    namespace = "com.okata.echelleeg39"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.echelle_eg_39"
+        applicationId = "com.okata.echelleeg39"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
